f0cd71a2d4db5431b50b28da424b1ff78cee4682
aesteve <arnaud.esteve@gmail.com>
2016-01-22 00:11:27 +0100

    very basic input + validate model

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -4,22 +4,35 @@ import Html.Events exposing (onClick, on, targetValue)
 import Signal exposing (Address)
 import StartApp.Simple as StartApp
 
+type alias Model =
+  { username : String
+  , typing : String
+  }
+
+init : Model
+init =
+  Model "Anonymous" "Anonymous"
 
 main =
-  StartApp.start { model = "Anonymous", view = view, update = update }
+  StartApp.start { model = init, view = view, update = update }
 
 type Action
   = PickUpName
   | UpdateName String
 
 update action model =
-  action
+  case action of
+    PickUpName ->
+      { model | username = model.typing }
+    UpdateName name ->
+      { model | typing = name }
 
 view address model =
   div []
-    [ input
-      [ value model
-      , on "input" targetValue (\string -> Signal.message address UpdateName string) ]
+    [ h1 [] [ text ("Hello " ++ model.username)]
+    , input
+      [ value model.typing
+      , on "input" targetValue (\val -> Signal.message address(UpdateName val)) ]
       []
     , button [ onClick address PickUpName ] [ text "Pick up username" ]
     ]
